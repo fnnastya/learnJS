d728da8dfdd1cbfb7015af5d2cf6f875aab484b5
fnnastya <44511849+fnnastya@users.noreply.github.com>
2019-06-01 02:06:10 +0300

    Add files via upload

--- practice01.js
+++ practice01.js
@@ -3,3 +3,21 @@ function add(x) {
     return (x+y);
   }
 }
+
+function makeCounter() {
+  var currentCount = 1;
+
+  return { 
+    count: function() {
+      return currentCount++;
+    },
+
+    set: function(value) {
+      return currentCount = value;
+    },
+
+    reset: function() {
+      return currentCount = 0;
+    }
+  };
+}
